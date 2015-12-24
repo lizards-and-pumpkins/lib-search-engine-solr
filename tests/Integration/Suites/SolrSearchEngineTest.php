60e062d45f8c625185564117ed33f44c46f619c2
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-12-24 23:12:14 +0100

    Issue #1: Add integration tests

---
+++ tests/Integration/Suites/SolrSearchEngineTest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace LizardsAndPumpkins;
+
+use LizardsAndPumpkins\ContentDelivery\Catalog\SortOrderConfig;
+use LizardsAndPumpkins\ContentDelivery\Catalog\SortOrderDirection;
+use LizardsAndPumpkins\ContentDelivery\FacetFieldTransformation\FacetFieldTransformationRegistry;
+use LizardsAndPumpkins\Context\Context;
+use LizardsAndPumpkins\Context\ContextBuilder;
+use LizardsAndPumpkins\Context\SelfContainedContextBuilder;
+use LizardsAndPumpkins\DataPool\SearchEngine\AbstractSearchEngineTest;
+use LizardsAndPumpkins\DataPool\SearchEngine\FacetFilterRequest;
+use LizardsAndPumpkins\DataPool\SearchEngine\SearchCriteria\SearchCriterionEqual;
+use LizardsAndPumpkins\DataPool\SearchEngine\SearchEngine;
+use LizardsAndPumpkins\DataPool\SearchEngine\Solr\Exception\SolrException;
+use LizardsAndPumpkins\DataPool\SearchEngine\Solr\Http\CurlSolrHttpClient;
+use LizardsAndPumpkins\DataPool\SearchEngine\Solr\Http\Exception\SolrConnectionException;
+use LizardsAndPumpkins\DataPool\SearchEngine\Solr\SolrSearchEngine;
+use LizardsAndPumpkins\Product\AttributeCode;
+
+class SolrSearchEngineTest extends AbstractSearchEngineTest
+{
+    /**
+     * @return Context
+     */
+    private function createTestContext()
+    {
+        return SelfContainedContextBuilder::rehydrateContext([
+            'website' => 'website',
+            'version' => '-1'
+        ]);
+    }
+
+    /**
+     * @param string $sortByFieldCode
+     * @param string $sortDirection
+     * @return SortOrderConfig
+     */
+    private function createTestSortOrderConfig($sortByFieldCode, $sortDirection)
+    {
+        return SortOrderConfig::createSelected(
+            AttributeCode::fromString($sortByFieldCode),
+            SortOrderDirection::create($sortDirection)
+        );
+    }
+
+    /**
+     * @param FacetFieldTransformationRegistry $facetFieldTransformationRegistry
+     * @return SearchEngine
+     */
+    final protected function createSearchEngineInstance(
+        FacetFieldTransformationRegistry $facetFieldTransformationRegistry
+    ) {
+        $testSolrConnectionPath = 'http://localhost:8983/solr/techproducts/';
+        $client = new CurlSolrHttpClient($testSolrConnectionPath);
+
+        $testSearchableAttributes = ['foo', 'baz'];
+
+        return new SolrSearchEngine(
+            $client,
+            $testSearchableAttributes,
+            $facetFieldTransformationRegistry
+        );
+    }
+
+    public function testExceptionIsThrownIfSolrQueryIsInvalid()
+    {
+        $nonExistingFieldCode = 'qux';
+        $fieldValue = 'whatever';
+
+        $facetFieldTransformationRegistry = new FacetFieldTransformationRegistry();
+        $searchEngine = $this->createSearchEngineInstance($facetFieldTransformationRegistry);
+
+        $searchCriteria = SearchCriterionEqual::create($nonExistingFieldCode, $fieldValue);
+
+        $expectedExceptionMessage = sprintf('undefined field %s', $nonExistingFieldCode);
+        $this->setExpectedException(SolrException::class, $expectedExceptionMessage);
+
+        $filterSelection = [];
+        $context = $this->createTestContext();
+        $facetFilterRequest = new FacetFilterRequest;
+        $rowsPerPage = 100;
+        $pageNumber = 0;
+        $sortOrderConfig = $this->createTestSortOrderConfig($nonExistingFieldCode, SortOrderDirection::ASC);
+
+        $searchEngine->getSearchDocumentsMatchingCriteria(
+            $searchCriteria,
+            $filterSelection,
+            $context,
+            $facetFilterRequest,
+            $rowsPerPage,
+            $pageNumber,
+            $sortOrderConfig
+        );
+    }
+
+    public function testExceptionIsThrownIfSolrIsNotAccessible()
+    {
+        $fieldCode = 'foo';
+        $fieldValue = 'bar';
+
+        $testSolrConnectionPath = 'http://localhost:8983/solr/nonexistingcore/';
+        $client = new CurlSolrHttpClient($testSolrConnectionPath);
+
+        $testSearchableAttributes = ['foo', 'baz'];
+        $facetFieldTransformationRegistry = new FacetFieldTransformationRegistry();
+
+        $searchEngine = new SolrSearchEngine(
+            $client,
+            $testSearchableAttributes,
+            $facetFieldTransformationRegistry
+        );
+
+        $expectedExceptionMessage = 'Error 404 Not Found';
+        $this->setExpectedException(SolrConnectionException::class, $expectedExceptionMessage);
+
+        $searchCriteria = SearchCriterionEqual::create($fieldCode, $fieldValue);
+        $filterSelection = [];
+        $context = $this->createTestContext();
+        $facetFilterRequest = new FacetFilterRequest;
+        $rowsPerPage = 100;
+        $pageNumber = 0;
+        $sortOrderConfig = $this->createTestSortOrderConfig($fieldCode, SortOrderDirection::ASC);
+
+        $searchEngine->getSearchDocumentsMatchingCriteria(
+            $searchCriteria,
+            $filterSelection,
+            $context,
+            $facetFilterRequest,
+            $rowsPerPage,
+            $pageNumber,
+            $sortOrderConfig
+        );
+    }
+}
---
+++ tests/Integration/bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+// Closure autoloader from
+// https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md#closure-example
+
+spl_autoload_register(function ($class) {
+    $prefix = 'LizardsAndPumpkins\\DataPool\\SearchEngine\\Solr\\';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    $relativeClass = substr($class, $len);
+    $file = __DIR__ . '/Suites/' . str_replace('\\', '/', $relativeClass) . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+$abstractSearchEngineTestLoader = function ($class) {
+    $prefix = 'LizardsAndPumpkins\\';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    $relativeClass = substr($class, $len);
+    $className = str_replace('\\', '/', $relativeClass);
+    $file = __DIR__ . '/../../vendor/lizards-and-pumpkins/catalog/tests/Unit/Suites/' . $className . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+};
+spl_autoload_register($abstractSearchEngineTestLoader);
